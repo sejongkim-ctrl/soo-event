076aa5cebc695f80dc51e3a014cd8b01d8fe43c0
sejongkim-ctrl <sejong.kim@medistream.co.kr>
2026-01-06 15:26:31 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const resultData = {
         desc: "당신의 관상은 목표를 향해 거침없이 돌진하는 <span class='highlight-txt'>'뭐든 밀어붙이는 상'</span>입니다.<br><br>2026년은 야망이 폭발하는 해이나, 과도한 열정으로 <span class='highlight-txt'>'엔진 과열'</span>이 우려됩니다. 천하를 얻으려면 무엇보다 <span class='highlight-txt'>'압도적인 에너지'</span>가 뒷받침되어야 합니다.",
         
         productSub: "지친 나를 위한",
-        productTitle: "원방공진단 수[壽]", // 닥터 수 처방 제품명
+        productTitle: "원방공진단 수壽", // 닥터 수 처방 제품명
         
         // 닥터 수의 처방
         productCopy: "당신의 지칠 줄 모르는 에너지는 부모님께 물려받은 유산이나, 정작 부모님의 엔진은 서서히 식어가고 있습니다.<br><br>부모님의 체력을 <span class='highlight-txt'>강력하게 끌어올릴 준비</span>를 해보세요.",
@@ -26,7 +26,7 @@ const resultData = {
         desc: "당신의 관상은 예리하고 지적인 책사이며, <span class='highlight-txt'>'가슴보다 머리가 지배하는 상'</span>입니다.<br><br>2026년은 두뇌 회전이 빨라지는 만큼 <span class='highlight-txt'>'뇌 과부하'</span>가 우려됩니다. 머리로 쏠린 열을 내리고 <span class='highlight-txt'>'흐트러짐 없는 총기'</span>를 유지하는 것이 만사형통의 열쇠입니다.",
         
         productSub: "흐릿해진 머리를 맑게",
-        productTitle: "사향공진단 수[壽]",
+        productTitle: "사향공진단 수壽",
         
         productCopy: "비상한 두뇌로 당신의 전략이 빛을 발하는 지금, 부모님의 기억은 세월 탓에 점차 흐릿해지고 있습니다.<br><br>막힌 기운을 뚫어주는 <span class='highlight-txt'>'사향'의 힘</span>을 선물해보세요.",
         
@@ -42,7 +42,7 @@ const resultData = {
         desc: "당신의 관상은 재물복이 타고난 <span class='highlight-txt'>'돈도 정, 사람도 정인 상'</span>입니다.<br><br>2026년은 더 높은 곳으로 비상할 기회이나, 화려한 겉모습 속에 내실을 놓칠 수 있습니다. <span class='highlight-txt'>'가장 근본적이고 귀한 것'</span>을 챙겨야 롱런할 수 있음을 명심하세요.",
         
         productSub: "가장 귀한 것을 선물하세요",
-        productTitle: "원방공진단 수[壽]",
+        productTitle: "원방공진단 수壽",
         
         productCopy: "당신의 귀티 나는 아우라는 부모님의 정성 덕분이나, 정작 부모님의 건강은 세월의 무게로 인해 약해지고 있습니다.<br><br>황제에게 진상하던 <span class='highlight-txt'>최고급 원료의 결정체</span>를 선물해보세요.",
         
@@ -58,7 +58,7 @@ const resultData = {
         desc: "당신의 관상은 인자하고 덕이 넘치며 <span class='highlight-txt'>'사람을 편하게 만드는 상'</span>입니다.<br><br>허나 모든 짐을 혼자 짊어지려는 책임감 때문에 2026년에는 <span class='highlight-txt'>'속앓이'</span>를 할 수 있습니다. 답답한 흐름을 뚫고 <span class='highlight-txt'>'마음의 평안'</span>을 찾는 것이 건강의 핵심입니다.",
         
         productSub: "답답한 속을 편안하게",
-        productTitle: "녹용경옥고 수[壽]",
+        productTitle: "녹용경옥고 수壽",
         
         productCopy: "타인을 배려하는 당신의 따뜻한 마음씨는 부모님을 닮았으나, 자식 걱정뿐인 부모님의 속은 지금 까맣게 타들어가고 있을 수 있어요.<br><br>녹용 성분으로 부모님의 <span class='highlight-txt'>꽉 막힌 기운을 부드럽게 순환</span>시켜 드려보세요.",
         
@@ -74,7 +74,7 @@ const resultData = {
         desc: "당신의 관상은 구속받지 않는 고고하며 <span class='highlight-txt'>'속세에 발 안 담그는 상'</span>입니다.<br><br>2026년에는 역마살과 함께 이동수가 많습니다. 낯선 환경에서도 탈이 나지 않으려면, 외부 풍파를 막아내는 <span class='highlight-txt'>'단단한 방어막(면역)'</span>이 필수입니다.",
         
         productSub: "면역력과 기초체력",
-        productTitle: "녹용경옥고 수[壽]",
+        productTitle: "녹용경옥고 수壽",
         
         productCopy: "자유로운 영혼은 부모님을 닮았으나, 당신이 밖을 누비는 동안 부모님의 몸은 찬 바람을 막아낼 힘을 잃어가고 있어요.<br><br>양기를 북돋우고 근골을 튼튼하게 하는 <span class='highlight-txt'>힘의 상징 '녹용'</span>을 부모님에게 선물해보세요.",
         
@@ -90,7 +90,7 @@ const resultData = {
         desc: "당신의 관상은 욕심 없이 평화로우며 <span class='highlight-txt'>'욕심이 빠진 평온한 상'</span>입니다.<br><br>다 좋으나 2026년에는 물 흐르듯 살다 자칫 활력이 떨어질 수 있습니다. 고인 물이 되지 않으려면 <span class='highlight-txt'>'마르지 않는 진액'</span>과 <span class='highlight-txt'>'생기'</span>를 채우는 것이 급선무입니다.",
         
         productSub: "마르지 않는 활력 충전",
-        productTitle: "녹용경옥고 수[壽]",
+        productTitle: "녹용경옥고 수壽",
         
         productCopy: "당신의 평온함은 부모님 덕분이나, 노화로 몸속 진액이 마른 부모님의 일상은 평화가 아닌 무기력함일 수 있습니다.<br><br>몸속 부족해진 진액을 보충하여, 메마른 부모님의 건강을 <span class='highlight-txt'>활력으로 채워주세요.</span>",
         
@@ -106,7 +106,7 @@ const resultData = {
         desc: "당신의 관상은 사람을 끌어당기는 도화살 가득한 <span class='highlight-txt'>'기운이 새어나오는 상'</span>입니다.<br><br>2026년에도 모임이 끊이지 않겠으나, 간과 체력이 비명을 지를 수 있습니다. <span class='highlight-txt'>노는 것도 집중력이 필수</span>입니다.",
         
         productSub: "번아웃 없는 집중력",
-        productTitle: "총명공진단 수[壽]", // 텍스트 내용에 맞춰 제품명 변경
+        productTitle: "총명공진단 수壽", // 텍스트 내용에 맞춰 제품명 변경
         
         productCopy: "업무도 노는 것도 '집중력'이 필요할 때!<br><br>피로회복 뿐만 아니라 작업기억 향상에 도움되는 <span class='highlight-txt'>총명공진단</span>을 스스로에게 선물하고 매일 아침 달라지는 가뿐함을 경험해보세요.",
         
@@ -122,7 +122,7 @@ const resultData = {
         desc: "당신의 관상은 감수성이 풍부한 <span class='highlight-txt'>'마음이 먼저 움직이는 상'</span>입니다.<br><br>2026년, 섬세한 감각으로 성취를 얻겠으나 감정 기복으로 기운이 약해질 수 있습니다. 강한 자극보다는 <span class='highlight-txt'>'부드럽게 스며드는 힘'</span>으로 몸을 보양해야 합니다.",
         
         productSub: "부드럽게 스며드는 보양",
-        productTitle: "부드러운 공진단 처방", // 상세 텍스트 맥락 반영
+        productTitle: "원방공진단 수壽", // 상세 텍스트 맥락 반영
         
         productCopy: "섬세한 감수성은 부모님을 닮았으나, 자식 걱정에 입맛마저 잃으신 부모님의 속은 지금 부드러운 위로가 필요합니다.<br><br>쓴맛은 줄이고 효능은 높여, <span class='highlight-txt'>입맛 없는 부모님도 부담 없이</span> 드실 수 있는 건강을 선물해보세요.",
         
