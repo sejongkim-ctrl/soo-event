aa8508c447e9c9d7132d796f2e71969a248f7c71
sejongkim-ctrl <sejong.kim@medistream.co.kr>
2026-01-06 14:49:41 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const resultData = {
         // 닥터 수의 처방
         productCopy: "당신의 지칠 줄 모르는 에너지는 부모님께 물려받은 유산이나, 정작 부모님의 엔진은 서서히 식어가고 있습니다.<br><br>부모님의 체력을 <span class='highlight-txt'>강력하게 끌어올릴 준비</span>를 해보세요.",
         
-        productImg: "./images/product_horse.jpg", 
+        productImg: "./images/product_a.jpg", 
         productUrl: "https://thesoo.co/promotion/26seol" 
     },
 
@@ -30,7 +30,7 @@ const resultData = {
         
         productCopy: "비상한 두뇌로 당신의 전략이 빛을 발하는 지금, 부모님의 기억은 세월 탓에 점차 흐릿해지고 있습니다.<br><br>막힌 기운을 뚫어주는 <span class='highlight-txt'>'사향'의 힘</span>을 선물해보세요.",
         
-        productImg: "./images/product_ai.jpg",
+        productImg: "./images/product_b.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
@@ -46,7 +46,7 @@ const resultData = {
         
         productCopy: "당신의 귀티 나는 아우라는 부모님의 정성 덕분이나, 정작 부모님의 건강은 세월의 무게로 인해 약해지고 있습니다.<br><br>황제에게 진상하던 <span class='highlight-txt'>최고급 원료의 결정체</span>를 선물해보세요.",
         
-        productImg: "./images/product_eagle.jpg",
+        productImg: "./images/product_c.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
@@ -62,7 +62,7 @@ const resultData = {
         
         productCopy: "타인을 배려하는 당신의 따뜻한 마음씨는 부모님을 닮았으나, 자식 걱정뿐인 부모님의 속은 지금 까맣게 타들어가고 있을 수 있어요.<br><br>녹용 성분으로 부모님의 <span class='highlight-txt'>꽉 막힌 기운을 부드럽게 순환</span>시켜 드려보세요.",
         
-        productImg: "./images/product_turtle.jpg",
+        productImg: "./images/product_d.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
@@ -78,7 +78,7 @@ const resultData = {
         
         productCopy: "자유로운 영혼은 부모님을 닮았으나, 당신이 밖을 누비는 동안 부모님의 몸은 찬 바람을 막아낼 힘을 잃어가고 있어요.<br><br>양기를 북돋우고 근골을 튼튼하게 하는 <span class='highlight-txt'>힘의 상징 '녹용'</span>을 부모님에게 선물해보세요.",
         
-        productImg: "./images/product_unicorn.jpg",
+        productImg: "./images/product_e.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
@@ -94,7 +94,7 @@ const resultData = {
         
         productCopy: "당신의 평온함은 부모님 덕분이나, 노화로 몸속 진액이 마른 부모님의 일상은 평화가 아닌 무기력함일 수 있습니다.<br><br>몸속 부족해진 진액을 보충하여, 메마른 부모님의 건강을 <span class='highlight-txt'>활력으로 채워주세요.</span>",
         
-        productImg: "./images/product_sloth.jpg",
+        productImg: "./images/product_d.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
@@ -110,7 +110,7 @@ const resultData = {
         
         productCopy: "업무도 노는 것도 '집중력'이 필요할 때!<br><br>피로회복 뿐만 아니라 작업기억 향상에 도움되는 <span class='highlight-txt'>총명공진단</span>을 스스로에게 선물하고 매일 아침 달라지는 가뿐함을 경험해보세요.",
         
-        productImg: "./images/product_zombie.jpg",
+        productImg: "./images/product_g.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
@@ -126,7 +126,7 @@ const resultData = {
         
         productCopy: "섬세한 감수성은 부모님을 닮았으나, 자식 걱정에 입맛마저 잃으신 부모님의 속은 지금 부드러운 위로가 필요합니다.<br><br>쓴맛은 줄이고 효능은 높여, <span class='highlight-txt'>입맛 없는 부모님도 부담 없이</span> 드실 수 있는 건강을 선물해보세요.",
         
-        productImg: "./images/product_sunfish.jpg",
+        productImg: "./images/product_h.jpg",
         productUrl: "https://thesoo.co/promotion/26seol"
     },
 
